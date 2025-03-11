41b4cb9c81e57f7282c52684f8eb8e8e731d579e
Pizzachomper <100880007+Pizzachomper@users.noreply.github.com>
2025-03-12 10:33:22 +1300

    Video 23

--- B_01_Colour_Quest_v1.py
+++ B_01_Colour_Quest_v1.py
@@ -77,20 +77,86 @@ class StartGame:
         self.start_frame = Frame(padx=10, pady=10)
         self.start_frame.grid()
 
+        # Strings for Labels
+        intro_string = "In each round you will be invited to choose a colour.\n"\
+                        "Your goal is to beat the target score and win the round (and keep your points)\n"\
+                        "To begin, please choose how many rounds you'd like to play" 
+
+        # Choose string = oops - please enter a whole number more than 0
+        choose_string = "How many rounds do you want to play?"
+
+        # list of labels to be made (text | font | fg)
+        start_labels_list = [
+            ["Colour quest", ("Arial", "16", "bold"), None],
+            [intro_string, ("Arial", "12"), None],
+            [choose_string, ("Arial", "12", "bold"), "#009900"]
+        ]
+
+        # Create labels and them to the refrence list
+
+        start_label_ref = []
+        for count, item in enumerate(start_labels_list):
+            make_label = Label(self.start_frame, text=item[0], font=item[1],
+                                fg=item[2],
+                                wraplength=350, justify="left", pady=10, padx=20)
+            make_label.grid(row=count)
+
+            start_label_ref.append(make_label)
+
+        # Extract choice label so that it can be changed to an error message if necessary
+        self.choose_label = start_label_ref[2]
+
+        # Frame so that entry box and button can be in the same row
+        self.entry_area_frame = Frame(self.start_frame)
+        self.entry_area_frame.grid(row=3)
+
+        self.num_rounds_entry = Entry(self.entry_area_frame, font=("Arial", "20", "bold"),
+                                        width=10)
+        self.num_rounds_entry.grid(row=0, column=0, padx=10, pady=10)
+
         # Create play button
-        self.play_button = Button(self.start_frame, font=("Arial", "16", "bold"),
+        self.play_button = Button(self.entry_area_frame, font=("Arial", "16", "bold"),
                                     fg="#FFFFFF", bg="#0057D8", text="Play", width=10,
                                     command=self.check_rounds)
         self.play_button.grid(row=0, column=1)
 
     def check_rounds(self):
-        """
-        Checks users have entered 1 or more rounds
-        """
+        # Retrieve number of rounds the user wants to play
+        rounds_wanted = self.num_rounds_entry.get()
 
-        Play(5)
-        # Hide root window (ie: hide round choice window)
-        root.withdraw()
+        # Reset label and entry box (for when users come to home screen)
+        self.choose_label.config(fg="#009900", font=("Arial", "12", "bold"))
+        self.num_rounds_entry.config(bg="#FFFFFF")
+
+        error = "Oops - please choose a whole number more than 0"
+        has_errors = "no"
+
+        # checks that amount to be converted is a number above absolute zero
+        try:
+            rounds_wanted = int(rounds_wanted)
+            if rounds_wanted > 0:
+                # Clear entry box and reset instruction label so that when users play a new game,
+                # they don't see an error message
+                self.num_rounds_entry.delete(0, END)
+                self.choose_label.config(text="How many rounds do you want to play?")
+                
+                # Invoke Play Class (and take across number of rounds)
+                Play(rounds_wanted)
+                # Hide root window (ie: hide rounds choice window)
+                root.withdraw()
+
+            else:
+                has_errors = "yes"
+        
+        except ValueError:
+            has_errors = "yes"
+        
+        # display the error if necessary
+        if has_errors == "yes":
+            self.choose_label.config(text=error, fg="#990000",
+                                     font=("Arial", "10", "bold"))
+            self.num_rounds_entry.config(bg="#F4CCCC")
+            self.num_rounds_entry.delete(0, END)
 
 
 class Play:
